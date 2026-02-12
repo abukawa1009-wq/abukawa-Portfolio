11bf63253efcd71125770e94d4b0321fdeb7ddce
abukawa <abukawa1009@gmail.com>
2026-02-12 12:59:14 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// タイトルがゆっくり動く
+const title = document.querySelector(".title");
+
+window.addEventListener("scroll", () => {
+  const y = window.scrollY;
+  title.style.transform = `translateY(${y * 0.2}px)`;
+});
+
+// Set Listスムーススクロール
+const links = document.querySelectorAll(".setlist a");
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute("href");
+    const target = document.querySelector(targetId);
+
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
+
+//画像スライダー
+const swiper = new Swiper(".swiper", {
+  // ページネーションが必要なら追加
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // ナビボタンが必要なら追加
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
